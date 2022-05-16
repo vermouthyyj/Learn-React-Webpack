dfa558d09c4b10e2dd96224a0fc84dc42577736c
vermouthyyj <yanyujun666@gmail.com>
2022-05-16 17:56:04 +0800

    static meme page

---
+++ Meme-Generator/components/Meme.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Meme() { 
+    return (
+        <main>
+            <form>
+                <input placeholder="Shut up"/>
+                <input placeholder="and take my money"/>
+            </form>
+            <button>Get a new meme image 🖼</button>
+        </main>
+    )
+}
